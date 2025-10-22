e3667d113587fa7953b33b966d3aba5043840a56
lkzcgfvf <lkzcgfvf2000@gmail.com>
2025-10-22 18:57:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request
+from time import gmtime, strftime
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    ip_addr = request.remote_addr
+    cur_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    return f"Hello World!<br>\n Its {cur_time} and your IP is {ip_addr}"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
